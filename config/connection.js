71091160bc72f892849e3f6714ab345a415d9d1b
Jonathan Parker <jprkr21@gmail.com>
2022-02-06 15:28:24 -0600

    updating port

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,10 @@
 const { connect, connection } = require('mongoose');
 
+// const connectionString = 
+//   process.env.MONGODB_URI || 'mongodb://localhost:3001/';
+
+const uri = process.env.MONGODB_URI;
+
 connect('mongodb://localhost/videosAndResponses', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
